2f93875e971fcc8df2fb063f9a53100d38ddcf52
FhLk <68800909+FhLk@users.noreply.github.com>
2023-04-27 15:58:46 +0700

    Update Design Complete

--- src/components/api.js
+++ src/components/api.js
@@ -5,76 +5,76 @@ class API {
   token =
     "eyJhbGciOiJIUzI1NiJ9.eyJtb2JpbGVObyI6Im1wUTBSMTJHTzAzNmY4ckVCbmZqVTg4OWwyczNnZGlGQUVzcCtNRWUrNzQ9IiwidGltZXN0YW1wIjoiMjAyMi0wMS0xNFQxMzowMDowNSswNzowMCJ9.gUvmq2MI9DAa5-AgWAX8DE7tL2elCD7VW8g-2gtYz9g";
   async Authen() {
-    // const res = await fetch(
-    //   "https://hitthemole-apistg.wisdomcloud.net/customer/ssbeserv/authenToken",
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       token: this.token,
-    //     }),
-    //   }
-    // );
-    // if (res.status === 200) {
-    //   const getRes = await res.json();
-    //   localStorage.setItem("transactionID", getRes.transactionID);
-    //   localStorage.setItem("token", getRes.data.token);
-    //   return
-    // }
+    const res = await fetch(
+      "https://hitthemole-apistg.wisdomcloud.net/customer/ssbeserv/authenToken",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: this.token,
+        }),
+      }
+    );
+    if (res.status === 200) {
+      const getRes = await res.json();
+      localStorage.setItem("transactionID", getRes.transactionID);
+      localStorage.setItem("token", getRes.data.token);
+      return
+    }
   }
 
   async RewardInfo() {
-    // const getToken = localStorage.getItem("token")
-    // const getID = localStorage.getItem("transactionID")
-    // LoadingAlert()
-    // const res = await fetch(
-    //   "https://hitthemole-apistg.wisdomcloud.net/game/rewardInfo",
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       Authorization: `Bearer ${getToken}`,
-    //       transactionid: getID,
-    //     },
-    //     body: JSON.stringify({
-    //       game: "hitTheMole",
-    //     }),
-    //   }
-    // );
-    // if(res.status === 200){
-    //     const getRes = await res.json()
-    //     localStorage.setItem("award",JSON.stringify({name:"",type:""}))
-    //     CloseAlert()
-    //     return getRes
-    // }
+    const getToken = localStorage.getItem("token")
+    const getID = localStorage.getItem("transactionID")
+    LoadingAlert()
+    const res = await fetch(
+      "https://hitthemole-apistg.wisdomcloud.net/game/rewardInfo",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getToken}`,
+          transactionid: getID,
+        },
+        body: JSON.stringify({
+          game: "hitTheMole",
+        }),
+      }
+    );
+    if(res.status === 200){
+        const getRes = await res.json()
+        localStorage.setItem("award",JSON.stringify({name:"",type:""}))
+        CloseAlert()
+        return getRes
+    }
   }
 
   async Play() {
-    // const getToken = localStorage.getItem("token")
-    // const getID = localStorage.getItem("transactionID")
-    // LoadingAlert()
-    // const res = await fetch(
-    //     "https://hitthemole-apistg.wisdomcloud.net/game/play",
-    //     {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //         Authorization: `Bearer ${getToken}`,
-    //         transactionid: getID,
-    //       },
-    //       body: JSON.stringify({
-    //         o: 0,
-    //         token: `Bearer ${getToken}`
-    //       }),
-    //     }
-    //   );
-    //   if(res.status === 200){
-    //     const getRes = await res.json();
-    //     CloseAlert()
-    //     return getRes
-    //   }
+    const getToken = localStorage.getItem("token")
+    const getID = localStorage.getItem("transactionID")
+    LoadingAlert()
+    const res = await fetch(
+        "https://hitthemole-apistg.wisdomcloud.net/game/play",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getToken}`,
+            transactionid: getID,
+          },
+          body: JSON.stringify({
+            o: 0,
+            token: `Bearer ${getToken}`
+          }),
+        }
+      );
+      if(res.status === 200){
+        const getRes = await res.json();
+        CloseAlert()
+        return getRes
+      }
   }
 }
 export default API;
